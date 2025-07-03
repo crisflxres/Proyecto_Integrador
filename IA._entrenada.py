80c2a367afc6f85089ee4f961609b0f8649f667a
Cristopher Armando Flores Bahena <cristopher_13240109235@uptecamac.edu.mx>
2025-07-02 22:37:19 -0600

    Agregamos las librerias de Pandas y Numpy para el manejo de los datos

---
+++ IA._entrenada.py
@@ -0,0 +1,7 @@
+import pandas as pd
+#Importamos la librería pandas para manejar los datos
+import numpy as np
+#Importamos la librería numpy para manejar los datos numéricos
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+#Importamos la función train_test_split para dividir los datos en conjuntos de entrenamiento y prueba
---
+++ p.py
@@ -0,0 +1,3 @@
+s = "                  Hello, World!       "
+s = s.strip()
+print(s)
