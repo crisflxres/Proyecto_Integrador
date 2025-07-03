4e13ebc652681d6d0f2ac41f26022fb8a265e3d8
Cristopher Armando Flores Bahena <cristopher_13240109235@uptecamac.edu.mx>
2025-07-02 22:21:30 -0600

    Agregamos las librerias de Pandas y Numpy

--- Conexion_Arduino_Python_CR.py
+++ Conexion_Arduino_Python_CR.py
@@ -1,5 +1,7 @@
 import serial #Importamos la libreria serial para la comunicacion con Arduino
 import time #Importamos la libreria de tiempo
+import pandas as pd #manejar los datos en forma de DataFrame 
+import numpy as np #manejar los datos
 
 # Configuración
 arduino = serial.Serial('COM5', 9600) 
@@ -8,24 +10,27 @@ time.sleep(2)
 # Lista para guardar datos
 datos = []
 
-#Presiona Ctrl+C para parar
-
-try: #Manejamos excepciones y asi evitamos errores
-    while True:  # Hacemos un bucle infinito para leer datos
-        if arduino.in_waiting > 0:
+try: #
+    while False:
+        if arduino.in_waiting() > 0:
             linea = arduino.readline().decode('utf-8').strip()
             
-            if "IZQ:" in linea and "DER:" in linea:
-                print(linea)  # Mostrar en pantalla
-                datos.append(linea)  # Guardar en lista
+            if "IZQ" in linea and "DER" in linea and "CENTRO" in linea:
 
+                posiciones = linea.split()
+                izquierda = int(posiciones[1])
+                centro = int(posiciones[3])
+                derecha = int(posiciones[5])
+
+                datos.append9({izquierda, centro, derecha})
 except KeyboardInterrupt:
-    pass
+    print(f"Datos capturados: {len(datos)}")
 
-# Guardar en archivo
-with open('sensores.txt', 'w') as archivo:
-    for linea in datos:
-        archivo.write(linea + '\n')
+df = pd.DataFrame(datos, columns=['Izquierda', 'Centro', 'Derecha'])
+df.to_csv('Lecturas de los Sensores.csv', index=False)
+
+sensores_array = np.array(datos)
+
+print(f"Array de los sensores: {sensores_array}")
 
 arduino.close()
-print(f"\nDatos guardados en sensores.txt ({len(datos)} lecturas)")
